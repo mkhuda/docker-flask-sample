1cadd4d2f72708d9f55b9cd1e3963637c28ee792
mkhuda <mhmmd.huda@gmail.com>
2017-10-22 02:03:53 +0700

    add useragents info

--- README.md
+++ README.md
@@ -2,3 +2,18 @@
 Docker container with Python 2.7 and Flask (Sample from Get Started)
 
 This container was bringin from Docker Get Started Page.
+
+## Libs Imported
+- Flask
+- Redis
+
+## Usages
+1. Clone this image
+2. Run `docker build -t <your_whathever_image_name> .` (ended with dot)
+3. Run `docker images` to check images
+4. Run `docker run -p 4000:80 <your_whathever_image_name>` or `docker run -d -p 4000:80 <your_whathever_image_name>`
+5. Locate to `http://localhost:4000` from your current browser
+Want to stop? Run `docker container stop <container_id>`
+To see list of container running, use `docker ps`.
+
+Thank you. And happy coding!
--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask
+from flask import Flask, request
 from redis import Redis, RedisError
+from user_agents import parse
 import os
 import socket
 
@@ -14,11 +15,29 @@ def hello():
         visits = redis.incr("counter")
     except RedisError:
         visits = "<i>cannot connect to Redis, counter disabled</i>"
+    
+    # grabbing user agent
+    ua_string = request.headers.get('User-Agent')
+    user_agent = parse(ua_string)
+    is_mobile = user_agent.is_mobile
+    is_tablet = user_agent.is_tablet
+    is_touch_capable = user_agent.is_touch_capable
+    is_pc = user_agent.is_pc
+    is_bot = user_agent.is_bot
+    string_user_agent = str(user_agent)
 
     html = "<h3>Hello! {name}!</h3>" \
            "<b>Hostname:</b> {hostname}<br/>" \
-           "<b>Visits:</b> {visits}"
-    return html.format(name="This message are from docker container!", hostname=socket.gethostname(), visits=visits)
+           "<b>Visits:</b> {visits}" \
+           "<br><br>" \
+           "<b>User Agent:</b> {ua}" \
+           "<b>Is mobile?</b> {is_mobile}" \
+           "<b>Is tablet?</b> {is_tablet}" \
+           "<b>Is touch capable ?</b> {is_touch_capable}" \
+           "<b>Is PC?</b> {is_pc}" \
+           "<b>Is bot? :D</b> {is_bot}" \
+           "<b>Your device is:</b> {string_user_agent}"
+    return html.format(name="This message are from docker container!", hostname=socket.gethostname(), visits=visits, ua=request.headers.get('User-Agent'), is_mobile=is_mobile, is_tablet = is_tablet, is_touch_capable = is_touch_capable, is_pc = is_pc, is_bot = is_bot, string_user_agent = string_user_agent)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80)
